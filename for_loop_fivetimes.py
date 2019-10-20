9dc17f1facd956a82e09c4ff92e70f647fafc643
anushanesargi <anushanesargi@gmail.com>
2019-10-20 22:28:55 +0100

    Loops

---
+++ for_loop_fivetimes.py
@@ -0,0 +1,27 @@
+#for loop
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times ' + str(i))
+
+#for loop using while loop
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times ' + str(i))
+    i = i + 1
+
+#for loop with start and end range
+print('My name is')
+for i in range(12, 16):
+    print('Jimmy Five Times ' + str(i))
+    
+
+#for loop with start, end and step
+print('My name is')
+for i in range(0, 10, 2):
+    print('Jimmy Five Times ' + str(i))
+
+#for loop with start, end and negative step
+print('My name is')
+for i in range(10, 0, -2):
+    print('Jimmy Five Times ' + str(i))
